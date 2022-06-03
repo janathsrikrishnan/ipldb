bc2421d9b5d486ca480734cd258b1389e044a8b2
janthJsk <janathjsk007@gmail.com>
2022-06-03 21:21:52 +0530

    point table created and editionNo and teamName(shortForm) used has primary key

---
+++ createPointTable.sql
@@ -0,0 +1,8 @@
+-- CREATE TABLE pointTable(editionNo int, TeamName varchar(30),
+-- 						played int, won int, lost int, Tied int, NR int,
+-- 						NetRunRate numeric(6,3),For_ varchar(30), Against varchar(30),
+-- 						points int);
+-- DROP TABLE pointTable;
+-- COPY pointTable FROM 'D:\ipldb\pointsTableEdited.csv' DELIMITER ',' CSV HEADER;
+-- SELECT * FROM pointTable;
+-- ALTER TABLE pointTable ADD CONSTRAINT pk PRIMARY KEY(editionNo, TeamName);
