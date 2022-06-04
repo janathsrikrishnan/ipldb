b375a3d864e03a60e0c6725f70b34042889b379a
janthJsk <janathjsk007@gmail.com>
2022-06-04 22:25:38 +0530

    basic search player page crated

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__, template_folder="template")
 
+
+# index page of the ipldb
 @app.route("/")
 def home():
     return render_template("index.html")
 
+# player info page
+@app.route("/player/<player_name>")
+def player(player_name):
+    return render_template("player.html", player_name = player_name)
+
+# about page of the ipldb
 @app.route("/about")
 def about():
     return render_template("about.html")
