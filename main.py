10286cb673e477172915c44788161c5b0fedb5c8
janthJsk <janathjsk007@gmail.com>
2022-06-04 18:15:55 +0530

    basic index and about page created

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from flask import Flask
+from flask import render_template
+app = Flask(__name__, template_folder="template")
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
