9fc38479f0e300fead4522d95e40323270f7df51
janthJsk <janathjsk007@gmail.com>
2022-06-04 17:26:23 +0530

    main file created

--- main.py
+++ main.py
@@ -1,19 +1,2 @@
-import psycopg2
-conn = psycopg2.connect(
-    host='localhost',
-    database='postgres',
-    user='postgres',
-    password='1234')
+from flask import Flask
 
-cur = conn.cursor()
-
-# getting version
-print("postgreSQL database version: ")
-cur.execute('SELECT version()')
-print(cur.fetchone())
-
-#checking table present
-try:
-    cur.execute('SELECT * FROM point_table')
-except:
-    print('table does not exit')
